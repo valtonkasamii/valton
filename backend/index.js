3c2e36b075fd1d39a7d52d79a944aeec64695742
valtonkasamii <valtonkasamii@outlook.com>
2024-12-31 17:14:21 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: 'https://valton-frontend.vercel.app',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
 }))
 app.use(cookieParser())
 
