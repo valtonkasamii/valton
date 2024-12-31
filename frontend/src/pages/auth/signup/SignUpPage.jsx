89cbf39a44bd0ce21dc6b92c560915e2b5cc3ec3
valtonkasamii <valtonkasamii@outlook.com>
2024-12-31 16:47:12 +0100

    Update SignUpPage.jsx

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -10,7 +10,7 @@ const SignUpPage = () => {
         e.preventDefault()
 
         try {
-        const response = await fetch("http://localhost:5000/api/auth/signup", {
+        const response = await fetch("https://valton.vercel.app/api/auth/signup", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({ username, password }),
