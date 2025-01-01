3910dc75a6a1a5f741f75bc3f37f4b8a6cdd4ba5
valtonkasamii <valtonkasamii@outlook.com>
2025-01-01 06:02:11 +0100

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.get("/me", protectRoute, getMe)
 router.post("/signup", signup)
+router.post("/login", login)
 router.post("/logout", logout)
 
 export default router
